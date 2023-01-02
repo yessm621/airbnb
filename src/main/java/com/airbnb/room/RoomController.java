d6d3f4f61ab051167f0886da404d3c9d1fdcb0bc
yessm621 <yessm621@gmail.com>
2023-01-02 17:53:30 +0900

    디자인 - 숙소 리스트

--- src/main/java/com/airbnb/room/RoomController.java
+++ src/main/java/com/airbnb/room/RoomController.java
@@ -1,9 +1,15 @@
 package com.airbnb.room;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/rooms")
 public class RoomController {
+
+    @GetMapping("/create")
+    public String room() {
+        return "/room/room_create";
+    }
 }
